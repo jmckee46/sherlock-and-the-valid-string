595cee8adee0e4b99d9c97863905842d4e583337
John McKee <jmckee3@mac.com>
2019-12-05 15:31:08 -0800

    wip

--- sherlock-and-the-valid-string.go
+++ sherlock-and-the-valid-string.go
@@ -1,3 +1,67 @@
 package main
 
+import "fmt"
+
+func isValid(s string) string {
+	sCount := make([]int32, 26)
+	sCountFrontLoaded := make([]int32, 26)
+
+	for _, value := range s {
+		sCount[int(value)-97]++
+	}
+
+	var j int32
+	for _, value := range sCount {
+		if value != 0 {
+			sCountFrontLoaded[j] = value
+			j++
+		}
+	}
+	sCountFiltered := sCountFrontLoaded[:j]
+	var matchFails int32
+	var suspect1 int32
+	var suspect2 int32
+	var base int32
+
+	// sCountFiltered: [2 2 1 1]
+
+	fmt.Println("sCountFiltered:", sCountFiltered)
+	for i := int32(1); i < j; i++ {
+		fmt.Println("i:", i, "matchFails", matchFails)
+		if matchFails == 0 {
+			if sCountFiltered[i-1] != sCountFiltered[i] {
+				fmt.Println("suspects collected")
+				suspect1 = sCountFiltered[i-1]
+				base = sCountFiltered[i-1]
+				suspect2 = sCountFiltered[i]
+				matchFails++
+			}
+		} else if matchFails == 1 {
+			fmt.Println("suspect1:", suspect1, "suspect2", suspect2, "sCountFiltered[i]", sCountFiltered[i])
+			if sCountFiltered[i] != suspect1 && sCountFiltered[i] != suspect2 {
+				fmt.Println("nobody matches!")
+				return "NO"
+			} else if sCountFiltered[i] == suspect1 && sCountFiltered[i] != suspect2 {
+				fmt.Println("suspect 2 is odd man")
+				if suspect2-1 != sCountFiltered[i] {
+					return "NO"
+				}
+			} else if sCountFiltered[i] != suspect1 && sCountFiltered[i] == suspect2 {
+				fmt.Println("suspect 1 is odd man")
+				if suspect1-1 != sCountFiltered[i] {
+					return "NO"
+				}
+			}
+			matchFails++
+		} else if matchFails == 2 {
+			if sCountFiltered[i-1] != sCountFiltered[i] {
+				return "NO"
+			}
+		}
+
+	}
+	// fmt.Println(sCount)
+	return "YES"
+}
+
 func main() {}
--- spec-sherlock-and-the-valid-string_test.go
+++ spec-sherlock-and-the-valid-string_test.go
@@ -2,6 +2,62 @@ package main
 
 import "testing"
 
-func TestSherlockAndTheValidString(t *testing.T) {
+func TestSherlockAndTheValidString1(t *testing.T) {
+	s := "aabbcd"
 
+	valid := isValid(s)
+
+	if valid != "NO" {
+		t.Errorf("got %s instead of NO", valid)
+	}
+}
+
+func TestSherlockAndTheValidString2(t *testing.T) {
+	s := "aabbccddeefghi"
+
+	valid := isValid(s)
+
+	if valid != "NO" {
+		t.Errorf("got %s instead of NO", valid)
+	}
+}
+
+func TestSherlockAndTheValidString3(t *testing.T) {
+	s := "abc"
+
+	valid := isValid(s)
+
+	if valid != "YES" {
+		t.Errorf("got %s instead of YES", valid)
+	}
+}
+
+func TestSherlockAndTheValidString4(t *testing.T) {
+	s := "abcc"
+
+	valid := isValid(s)
+
+	if valid != "YES" {
+		t.Errorf("got %s instead of YES", valid)
+	}
+}
+
+func TestSherlockAndTheValidString5(t *testing.T) {
+	s := "abccc"
+
+	valid := isValid(s)
+
+	if valid != "NO" {
+		t.Errorf("got %s instead of NO", valid)
+	}
+}
+
+func TestSherlockAndTheValidString6(t *testing.T) {
+	s := "a"
+
+	valid := isValid(s)
+
+	if valid != "YES" {
+		t.Errorf("got %s instead of YES", valid)
+	}
 }
