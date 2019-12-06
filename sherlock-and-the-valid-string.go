9bb6b4e50ff7bcdaffad22c400c7a9f41470084c
John McKee <jmckee3@mac.com>
2019-12-05 16:41:35 -0800

    working version with logging

--- sherlock-and-the-valid-string.go
+++ sherlock-and-the-valid-string.go
@@ -6,10 +6,13 @@ func isValid(s string) string {
 	sCount := make([]int32, 26)
 	sCountFrontLoaded := make([]int32, 26)
 
+	// count frequencies
 	for _, value := range s {
 		sCount[int(value)-97]++
 	}
+	fmt.Println("sCount:", sCount)
 
+	// filter out 0's
 	var j int32
 	for _, value := range sCount {
 		if value != 0 {
@@ -18,50 +21,60 @@ func isValid(s string) string {
 		}
 	}
 	sCountFiltered := sCountFrontLoaded[:j]
-	var matchFails int32
-	var suspect1 int32
-	var suspect2 int32
-	var base int32
-
-	// sCountFiltered: [2 2 1 1]
-
 	fmt.Println("sCountFiltered:", sCountFiltered)
+
+	// determin if valid
+	var matchFails int32
+	var base = sCountFiltered[0]
+	var baseConfirmed bool
+
 	for i := int32(1); i < j; i++ {
-		fmt.Println("i:", i, "matchFails", matchFails)
-		if matchFails == 0 {
-			if sCountFiltered[i-1] != sCountFiltered[i] {
-				fmt.Println("suspects collected")
-				suspect1 = sCountFiltered[i-1]
-				base = sCountFiltered[i-1]
-				suspect2 = sCountFiltered[i]
+
+		fmt.Println("")
+		fmt.Println("baseConfirmed:", baseConfirmed)
+		fmt.Println("base:", base)
+		fmt.Println("sCountFiltered[i]:", sCountFiltered[i])
+		fmt.Println("matchFails:", matchFails)
+
+		if base != sCountFiltered[i] {
+			if absValue(base-sCountFiltered[i]) > 1 {
+				if sCountFiltered[i] == int32(1) {
+					fmt.Println("special case = 1")
+					matchFails++
+				} else {
+					fmt.Println("more than 1 apart")
+					matchFails = 2
+				}
+				break
+			} else if !baseConfirmed && base-1 == sCountFiltered[i] {
+				base = sCountFiltered[i]
+				matchFails++
+			} else if base == sCountFiltered[i]-1 {
+				fmt.Println("decrement one scenario")
+				matchFails++
+			} else {
+				fmt.Println("incrementing matchFails")
 				matchFails++
 			}
-		} else if matchFails == 1 {
-			fmt.Println("suspect1:", suspect1, "suspect2", suspect2, "sCountFiltered[i]", sCountFiltered[i])
-			if sCountFiltered[i] != suspect1 && sCountFiltered[i] != suspect2 {
-				fmt.Println("nobody matches!")
-				return "NO"
-			} else if sCountFiltered[i] == suspect1 && sCountFiltered[i] != suspect2 {
-				fmt.Println("suspect 2 is odd man")
-				if suspect2-1 != sCountFiltered[i] {
-					return "NO"
-				}
-			} else if sCountFiltered[i] != suspect1 && sCountFiltered[i] == suspect2 {
-				fmt.Println("suspect 1 is odd man")
-				if suspect1-1 != sCountFiltered[i] {
-					return "NO"
-				}
-			}
-			matchFails++
-		} else if matchFails == 2 {
-			if sCountFiltered[i-1] != sCountFiltered[i] {
-				return "NO"
-			}
+		} else {
+			fmt.Println("equal and base confirmed")
+			baseConfirmed = true
 		}
 
 	}
-	// fmt.Println(sCount)
+	if matchFails > 1 {
+		return "NO"
+	}
+
 	return "YES"
+}
+
+func absValue(x int32) int32 {
+	if x > 0 {
+		return x
+	}
+
+	return -x
 }
 
 func main() {}
